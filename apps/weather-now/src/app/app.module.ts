d4be0d7ecdfb03b8b1a926b93be84d2e76d79338
Rafael Nunes <rafael.nunes.dev@gmail.com>
2022-03-22 21:14:28 -0300

    Commit inicial

--- apps/weather-now/src/app/app.module.ts
+++ apps/weather-now/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CardComponent } from './components/card/card.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, CardComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
